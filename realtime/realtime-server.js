217a00b02d131d47ae3d29013297dfe878bd6045
Peter Wang <happypeter1983@gmail.com>
2014-05-30 15:27:20 +0800

    webpage send username, nodejs save it to redis

--- realtime/realtime-server.js
+++ realtime/realtime-server.js
@@ -2,14 +2,14 @@
 var io = require('socket.io').listen(5001),
     redis = require('redis').createClient();
 
-redis.set("peter", "billie");
-redis.get("peter", function(err, reply){
-	console.log(reply +"++++++++++++++");
-})
 io.on('connection', function(socket){
   console.log('......a user connected.................');
   socket.on('joiner-name', function(message) {
   	console.log(message)
+	redis.set("username", message);
+	redis.get("username", function(err, reply){
+		console.log(reply +"++++++++++++++");
+	})
   });
 });
 
