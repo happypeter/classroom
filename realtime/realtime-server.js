74d13b379b39593fe44b7c438718e5c02d91cf75
Peter Wang <happypeter1983@gmail.com>
2014-05-30 13:56:28 +0000

    online memeber shown correctly

--- realtime/realtime-server.js
+++ realtime/realtime-server.js
@@ -2,26 +2,51 @@
 var io = require('socket.io').listen(5001),
     redis = require('redis').createClient();
 
+
+// usernames which are currently connected to the chat
+ var usernames = {};
+ var numUsers = 0;
+
 io.on('connection', function(socket){
+  var addedUser = false;
+
+
   console.log('......a user connected.................');
   socket.on('joiner-name', function(message) {
-  	console.log(message)
-	redis.set("username", message);
-	redis.get("username", function(err, reply){
-		console.log(reply +"++++++++++++++");
-	})
+    console.log(message)
+    redis.set("username", message);
+  redis.get("username", function(err, reply){
+    console.log(reply +"++++++++++++++");
+  })
   });
 
 
-   socket.on('add user', function (username) {
-		console.log("+++on add user+++++++++++");
+  socket.on('add user', function (username) {
+    addedUser = true;
+    socket.username = username;
+    // add the client's username to the global list
+    usernames[username] = username;
+    ++numUsers;
+
     socket.broadcast.emit('user joined', {
-      username: username,
-      numUsers: "3"
+      username: socket.username,
+      numUsers: numUsers
     });
+  });
+
+   socket.on('disconnect', function () {
+     // remove the username from global usernames list
+     if (addedUser) {
+       delete usernames[socket.username];
+       --numUsers;
+     }
+     
+     // echo globally that this client has left
+     socket.broadcast.emit('user left', {
+       username: socket.username,
+       numUsers: numUsers
+     });
    });
-
-
 
 });
 
