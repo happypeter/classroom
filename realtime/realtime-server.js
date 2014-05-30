d8b3d832c0705364737fa781a1e33e331aea67f4
Peter Wang <happypeter1983@gmail.com>
2014-05-30 15:15:29 +0800

    send username from webpage to nodejs sever

--- realtime/realtime-server.js
+++ realtime/realtime-server.js
@@ -2,13 +2,15 @@
 var io = require('socket.io').listen(5001),
     redis = require('redis').createClient();
 
-redis.subscribe('rt-change');
-
+redis.set("peter", "billie");
+redis.get("peter", function(err, reply){
+	console.log(reply +"++++++++++++++");
+})
 io.on('connection', function(socket){
   console.log('......a user connected.................');
-  redis.on('message', function(channel, message){
-      socket.emit('rt-change', JSON.parse(message));
-      console.log('......... redis.on message ...................:' );
-    });
+  socket.on('joiner-name', function(message) {
+  	console.log(message)
+  });
 });
+
 
