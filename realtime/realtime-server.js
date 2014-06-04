de7853a9a964c6fd9284100605c83abf65265e21
Peter Wang <happypeter1983@gmail.com>
2014-06-04 11:55:13 +0800

    save online offline time

--- realtime/realtime-server.js
+++ realtime/realtime-server.js
@@ -3,15 +3,27 @@ var io = require('socket.io').listen(5001),
     redis = require('redis').createClient();
 
 
+if (redis.exists("connect_id")) {
+  redis.del("connect_id");
+}
+
 // usernames which are currently connected to the chat
  var usernames = {};
  var numUsers = 0;
 
-function saveRedis(message) {
-  console.log(message)
-  redis.set("username", message);
-  redis.get("username", function(err, reply){
-    console.log("...function....redis reply:" + reply);
+function saveOnlineTime(username) {
+  redis.incr("connect_id", function(err, id){
+     var t = new Date();
+     redis.hset("connect:" + id, "online", t.getTime());
+     redis.lpush("connect_id:billie", id);
+  });
+}
+
+function saveOfflineTime(username) {
+  var t = new Date();
+  redis.lpop("connect_id:billie", function(err, id){
+    redis.hset("connect:" + id, "offline", t.getTime());
+    redis.lpush("connects:billie", "connect:" + id);
   })
 }
 
@@ -24,7 +36,7 @@ io.on('connection', function(socket){
     // add the client's username to the global list
     usernames[username] = username;
     ++numUsers;
-    saveRedis(username);
+    saveOnlineTime(username);
 
     io.sockets.emit('user joined', {
       username: socket.username,
@@ -39,7 +51,7 @@ io.on('connection', function(socket){
        delete usernames[socket.username];
        --numUsers;
      }
-     
+     saveOfflineTime(socket.username);
      // echo globally that this client has left
      socket.broadcast.emit('user left', {
        username: socket.username,
